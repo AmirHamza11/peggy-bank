7cc24ccb974ad1cb4b2d870d17f5737f861321c2
AmirHamza11 <amirhamza11333@gmail.com>
2023-05-30 21:11:15 +0600

    some packages

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
